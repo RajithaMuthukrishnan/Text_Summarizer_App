164be5dadffc571f7bef1be50bc58b738c4dbde6
Rajitha Muthukrishnan <rajitha.muthukrishnan@gmail.com>
2023-08-03 15:49:22 +0000

    Use Model Inference API

--- frontend/homepage/views.py
+++ frontend/homepage/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.apps import apps
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+import requests
 
 
 global text_data, summary, button_text
+API_URL = "https://api-inference.huggingface.co/models/RajithaMuthukrishnan/text-summariser-english"
+headers = {"Authorization": "Bearer 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"}
 
 # Create your views here.
 def homepage(request):
@@ -19,8 +22,7 @@ def get_data(request):
     global button_text
     if (button_text=='Submit' and request.method == 'POST'):
         text_data = request.POST['doc_text_area'] 
-        summary = predict(text_data)[0]
-        # summary = ['Yeayyyyyyy its working!!!'][0]
+        summary = predict(text_data)
         button_text = "Clear"
         context = {
             'text_data': text_data,
@@ -32,18 +34,16 @@ def get_data(request):
     if (button_text=='Clear'):
         return HttpResponseRedirect(reverse('homepage'))
 
+
+# Helper Functions
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
 def predict(text_data):
     print('Predicting...')
-    inputs = apps.get_app_config('homepage').tokenizer(
-        text_data,
-        padding='max_length',
-        truncation=True,
-        max_length=apps.get_app_config('homepage').encoder_max_length,
-        return_tensors='pt',
-    )
-    input_ids = inputs.input_ids.to(apps.get_app_config('homepage').model.device)
-    attention_mask = inputs.attention_mask.to(apps.get_app_config('homepage').model.device)
-    outputs = apps.get_app_config('homepage').model.generate(input_ids, attention_mask=attention_mask)
-    output_str = apps.get_app_config('homepage').tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    output = query({"inputs": text_data,})
     print('Done!')
+    output_dict = output[0]
+    output_str = output_dict.get('generated_text')
     return output_str
